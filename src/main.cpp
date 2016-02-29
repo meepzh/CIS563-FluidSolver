96271f37b67fd5705c3eef7bedb68a3961ae5b1a
Meep Zhou <meep.zh@gmail.com>
2016-02-28 20:03:44 -0500

    changed cmake a bit according to tutorials

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
 //  main.cpp
 //  MFluidSolver
 
+#include <stdio.h>
 #include "main.hpp"
+#include "MFluidSolverConfig.hpp"
 
 using namespace std;
 
 int main() {
+    fprintf(stdout,"Version %d.%d\n", MFluidSolver_VERSION_MAJOR, MFluidSolver_VERSION_MINOR);
     return 0;
 }
